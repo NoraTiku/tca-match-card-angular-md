7516656e59250023d30e0ec6f1e9889a11292cb1
NoraTiku <jtiku@madisoncollege.edu>
2022-04-04 12:57:26 -0500

    Generated new components..
    ng generate component home --skip-tests
    
    ng generate component play-game --skip-tests
    
    ng generate component set-up --skip-tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { SetUpComponent } from './set-up/set-up.component';
+import { PlayGameComponent } from './play-game/play-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SetUpComponent,
+    PlayGameComponent
   ],
   imports: [
     BrowserModule,
