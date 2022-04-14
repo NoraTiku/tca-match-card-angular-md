cfb87fd0f7649015caf6111e9c32185ec1d41bfe
NoraTiku <jtiku@madisoncollege.edu>
2022-04-14 15:14:06 -0500

    Got game result working...

--- src/app/card-game.service.ts
+++ src/app/card-game.service.ts
@@ -3,10 +3,47 @@ import { Injectable } from '@angular/core';
 export interface gameResult {
   start: string;
   end: string;
-  win: string;
-  myTurns: string[];
+  //win: string;
+  //myTurns: string[];
   
 }
+
+
+
+export interface player {
+  name: string;
+  order: number;
+};
+export interface gameResult {
+  start: string;      // "2022-02-14T18:49:30"
+  end: string;        // "2022-02-14T18:59:30"
+  winner: string;      // "Me"
+  players: player[] ;  
+
+
+  // tca-zombie-specific
+  expansions?: any[];
+  gameTurns?: any[]
+};
+
+
+
+
+const game1: gameResult = {
+  start: "2022-02-14T18:55:00"
+  , end: "2022-02-14T19:00:00"
+  , winner: "Me"
+  , players: [{ name: "Me", order: 1 }, { name: "Jack", order: 2 }, { name: "Taylor", order: 3 }]
+  
+  
+};
+
+const game2: gameResult = {
+  start: "2022-02-14T19:05:00"
+  , end: "2022-02-14T19:35:00"
+  , winner: "Stephanie"
+  , players: [{ name: "Me", order: 1 }, { name: "Stephanie", order: 2 }]
+};
 
 
 
@@ -15,83 +52,24 @@ export interface gameResult {
 })
 export class CardGameService {
 
-  constructor() { }
+  constructor() { 
+    
+    
+  }
+  gameResults: gameResult[] = [
+    game1
+    , game2
+];
+
+addGameResult = (r: gameResult) => {
+  this.gameResults = [
+    ...this.gameResults,
+    r
+  ];
+};
 
   
   
-   gameResults: gameResult[] = [
-    {
-      start: "2022-04-14T18:30:00"
-      , end: "2022-04-14T18:00:00"
-      , win: "me"
-      , myTurns: [
-        "match"
-        , "no match"
-        , "no match"
-        , "match"
-      ]
-    }
-    , {
-      start: ""
-      , end: ""
-      , win: "Bless"
-      , myTurns: [
-          "match"
-          , "match"
-          , "match"
-          , "no match"
-          , "no match"
-          , "no match"
-          , "no match"
-          , "no match"
-          , "match"
-          , "match"
-          , "match"
-          , "match"
-          , "match"
-          , "match"
-          , "match"
-          , "match"
-          , "match"
-          , "match"
-          , "no match"
-          , "no match"
-          , "no match"
-          , "no match"
-          , "no match"
-          , "no match"
-          , "match"
-          , "match"
-          , "match"
-          , "match"
-          , "match"
-          , "match"
-          , "match"
-          , "match"
-          , "match"
-          , "match"
-          , "match"
-      ]
-    }
-    , {
-      start: ""
-      , end: ""
-      , win:" Joel"
-      , myTurns: [
-        "match"
-        , "no match"
-        , "no match"
-        , "match"
-      ]
-    }];
-
-    addGameResult = (r: gameResult ) => {
-      this.gameResults = [
-        ...this.gameResults, 
-        r
-
-      ];
-    };
   
  
   
--- src/app/play-game/play-game.component.ts
+++ src/app/play-game/play-game.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CardGameService, gameResult } from '../card-game.service';
+import {Router } from '@angular/router';
 
 @Component({
   selector: 'app-play-game',
@@ -8,17 +9,27 @@ import { CardGameService, gameResult } from '../card-game.service';
 })
 export class PlayGameComponent implements OnInit {
 
-  constructor(private gameSVC: CardGameService) { }
+  constructor(private gameSVC: CardGameService,
+    private router: Router
+
+    ) { }
 
   ngOnInit(): void {
   }
 
-  endGame = (r: gameResult) => {
+  endGame = () => {
     //add new game result to the service
+    this.gameSVC.addGameResult({
+      start: "",
+      end: "",
+      players: [],
+      winner: ""
+    });
 
 
     //navigate back to the home
-  }
+    this.router.navigateByUrl("home");
+  };
 
 }
 
